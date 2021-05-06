8d1286471c88a0d258187e2bca0d9ad1cecfd20b
snake-whisperer <79474323+snake-whisperer@users.noreply.github.com>
2021-05-06 15:22:33 -0500

    change home to index; delete static folder in calc directory

--- calc/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django import forms
-
-
-#class TextBoxGen(forms.Form):
-#    vent_size = forms.FloatField(max_length=4)
-
-#    class Meta:
-#        fields = ['vent_size']
